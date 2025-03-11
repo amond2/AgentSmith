c0cf5c94ae2d3ac40a881b367fff9e859172aa0b
amond2 <8hyojae@gmail.com>
2025-03-11 15:28:23 +0900

    feat: 점프 구현

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    [Header("Player Movement")]
+    private Rigidbody rigidbody;
+    private float moveSpeed = 5f;
+    private float jumpForce = 7f;
+    private bool isGrounded = false;
+    
+    [Header("Mouse Look Settings")]
+    public Transform cameraTransform;
+    private float mouseSensitivity = 2f;
+    private float rotationX = 0f;
+
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        MouseLook();
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && isGrounded)
+        {
+            Debug.Log("Jump Pressed");
+            Jump();
+        }
     }
+
+    private void Move()
+    {
+        float moveX = Keyboard.current.aKey.isPressed ? -1 : Keyboard.current.dKey.isPressed ? 1 : 0;
+        float moveZ = Keyboard.current.sKey.isPressed ? -1 : Keyboard.current.wKey.isPressed ? 1 : 0;
+
+        Vector3 moveDirection = cameraTransform.forward * moveZ + cameraTransform.right * moveX;
+        moveDirection.y = 0f;
+
+        rigidbody.velocity = new Vector3(moveDirection.x * moveSpeed, rigidbody.velocity.y, moveDirection.z * moveSpeed);
+    }
+    
+    private void MouseLook()
+    {
+        float mouseX = Mouse.current.delta.x.ReadValue() * mouseSensitivity;
+        float mouseY = Mouse.current.delta.y.ReadValue() * mouseSensitivity;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+    }
+
+    private void Jump()
+    {
+        rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        isGrounded = false;
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    
 }
